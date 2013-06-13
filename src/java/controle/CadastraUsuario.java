7be1a57356099e8959d9c9fb1f3f12c7e873ac42
Hugo Nomura <hugonomura@gmail.com>
2013-06-12 23:15:43 -0300

    Versao inicial do tutorial.

--- src/java/controle/CadastraUsuario.java
+++ src/java/controle/CadastraUsuario.java
@@ -86,7 +86,7 @@ public class CadastraUsuario extends HttpServlet {
         u.setUsuario(request.getParameter("username"));
         u.setTipo("user");
         u.setEmail(request.getParameter("email"));
-        u.setSenha(request.getParameter("senha"));
+        u.setSenha(request.getParameter("pwd"));
         u.setCidade(request.getParameter("sexo"));
         u.setSexo(request.getParameter("cidade"));
         
